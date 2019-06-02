71088b8e5ceb24c54ddfa8efc2d63dd9d8a2f697
wangquanzhou <wangquanzhou@nibirutech.com>
2019-06-02 14:29:11 +0800

    add zuul 2019年06月02日14:29:10

---
+++ zuul-service/src/main/java/com/audi/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package com.audi.zuul;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@Slf4j
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+        log.info("zuul service started success...");
+    }
+}
