764404e62f13a8714625642ec47c1ca2f80ba2a5
wangquanzhou <wangquanzhou@nibirutech.com>
2019-06-15 17:08:54 +0800

    修改dockerfile  使用本地harbor的image  修改根路径下build.sh使其可以推送镜像到harbor 2019年06月15日17:08:53

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 mvn clean package
 
-docker image rm -f zuul-service
-docker build -t zuul-service:latest ./zuul-service/
+docker image rm -f hub.audi.com:1010/micro-service/zuul-service
+docker build -t hub.audi.com:1010/micro-service/zuul-service:latest ./zuul-service/
+docker push hub.audi.com:1010/micro-service/zuul-service:latest
 
-docker image rm -f user-service
-docker build -t user-service:latest ./user-service/
+docker image rm -f hub.audi.com:1010/micro-service/user-service
+docker build -t hub.audi.com:1010/micro-service/user-service:latest ./user-service/
+docker push hub.audi.com:1010/micro-service/user-service:latest
 
-docker image rm -f eureka-service
-docker build -t eureka-service:latest ./eureka-service/
+docker image rm -f hub.audi.com:1010/micro-service/eureka-service
+docker build -t hub.audi.com:1010/micro-service/eureka-service:latest ./eureka-service/
+docker push hub.audi.com:1010/micro-service/eureka-service:latest
 
-docker image rm -f infrastructure-service
-docker build -t infrastructure-service:latest ./infrastructure-service/
+docker image rm -f hub.audi.com:1010/micro-service/infrastructure-service
+docker build -t hub.audi.com:1010/micro-service/infrastructure-service:latest ./infrastructure-service/
+docker push hub.audi.com:1010/micro-service/infrastructure-service:latest
