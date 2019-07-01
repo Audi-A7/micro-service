41c2412a87a86410ace71b3649387ae109586770
wangquanzhou <wangquanzhou@nibirutech.com>
2019-07-01 18:10:12 +0800

    add config 2019年07月01日18:10:11

---
+++ harbor/readme.txt
@@ -0,0 +1,5 @@
+启动：切换到/Users/wangquanzhou/soft/harbor目录  首次启动需要执行./install.sh，后续启动可以使用 docker-compose up -d
+
+登录地址：http://hub.audi.com:1010
+用户名：admin
+密码：Harbor12345
---
+++ portainer/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+docker run -d -p 9000:9000 \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --name prtainer-test \
+    portainer/portainer:1.21.0
