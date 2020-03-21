203496f1bd6e459fd5222c5dd0e311ece5a604c4
wangquanzhou <audi.car@qq.com>
2020-03-21 21:22:09 +0800

    fix swarm config

--- build-ali.sh
+++ build-ali.sh
@@ -5,22 +5,22 @@
 
 mvn clean package
 
-docker image rm -f 39.101.191.157:1010/micro-service/zuul-service
-docker build -t 39.101.191.157:1010/micro-service/zuul-service:latest ./zuul-service/
-docker push 39.101.191.157:1010/micro-service/zuul-service:latest
+docker image rm -f registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/zuul-service
+docker build -t registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/zuul-service:latest ./zuul-service/
+docker push registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/zuul-service:latest
 
-docker image rm -f 39.101.191.157:1010/micro-service/user-service
-docker build -t 39.101.191.157:1010/micro-service/user-service:latest ./user-service/
-docker push 39.101.191.157:1010/micro-service/user-service:latest
+docker image rm -f registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/user-service
+docker build -t registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/user-service:latest ./user-service/
+docker push registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/user-service:latest
 
-docker image rm -f 39.101.191.157:1010/micro-service/eureka-service
-docker build -t 39.101.191.157:1010/micro-service/eureka-service:latest ./eureka-service/
-docker push 39.101.191.157:1010/micro-service/eureka-service:latest
+docker image rm -f registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/eureka-service
+docker build -t registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/eureka-service:latest ./eureka-service/
+docker push registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/eureka-service:latest
 
-docker image rm -f 39.101.191.157:1010/micro-service/infrastructure-service
-docker build -t 39.101.191.157:1010/micro-service/infrastructure-service:latest ./infrastructure-service/
-docker push 39.101.191.157:1010/micro-service/infrastructure-service:latest
+docker image rm -f registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/infrastructure-service
+docker build -t registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/infrastructure-service:latest ./infrastructure-service/
+docker push registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/infrastructure-service:latest
 
-docker image rm -f 39.101.191.157:1010/micro-service/gateway-service
-docker build -t 39.101.191.157:1010/micro-service/gateway-service:latest ./gateway-service/
-docker push 39.101.191.157:1010/micro-service/gateway-service:latest
+docker image rm -f registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/gateway-service
+docker build -t registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/gateway-service:latest ./gateway-service/
+docker push registry.cn-shanghai.aliyuncs.com/audi_micro_service/micro-service/gateway-service:latest
---
+++ build-harbor-ali.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# 便衣本地的package  并且声称tag  并且推送到harbor仓库
+
+ source ~/.bash_profile
+
+mvn clean package
+
+
+docker image rm -f 39.101.191.157:1010/micro-service/zuul-service
+docker build -t 39.101.191.157:1010/micro-service/zuul-service:latest ./zuul-service/
+docker push 39.101.191.157:1010/micro-service/zuul-service:latest
+
+docker image rm -f 39.101.191.157:1010/micro-service/user-service
+docker build -t 39.101.191.157:1010/micro-service/user-service:latest ./user-service/
+docker push 39.101.191.157:1010/micro-service/user-service:latest
+
+docker image rm -f 39.101.191.157:1010/micro-service/eureka-service
+docker build -t 39.101.191.157:1010/micro-service/eureka-service:latest ./eureka-service/
+docker push 39.101.191.157:1010/micro-service/eureka-service:latest
+
+docker image rm -f 39.101.191.157:1010/micro-service/infrastructure-service
+docker build -t 39.101.191.157:1010/micro-service/infrastructure-service:latest ./infrastructure-service/
+docker push 39.101.191.157:1010/micro-service/infrastructure-service:latest
+
+docker image rm -f 39.101.191.157:1010/micro-service/gateway-service
+docker build -t 39.101.191.157:1010/micro-service/gateway-service:latest ./gateway-service/
+docker push 39.101.191.157:1010/micro-service/gateway-service:latest
